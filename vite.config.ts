15070726c5cac4cf74aa41f1d1d612d8e3abacd2
Baron <qbmrodrigo@gmail.com>
2023-06-15 16:02:26 +0800

    Remove base

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,6 @@ import IconsResolver from 'unplugin-icons/resolver'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '',
   plugins: [
     VueRouter({
       dts: 'types/typed-router.d.ts',
